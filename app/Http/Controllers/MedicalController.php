77326c858046979cb2a2f90fc66b33b388c5ee7b
CHIHAB09 <web2020.chihab@gmail.com>
2021-05-05 16:02:04 +0200

    relation many to many toujours le soucie dessus + affichage des resultats

--- app/Http/Controllers/MedicalController.php
+++ app/Http/Controllers/MedicalController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Medical;
 use Illuminate\Http\Request;
 
 class MedicalController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Medical  $medical
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Medical $medical)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Medical  $medical
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Medical $medical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Medical  $medical
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Medical $medical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Medical  $medical
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Medical $medical)
+    {
+        //
+    }
 }
--- app/Http/Controllers/ParcController.php
+++ app/Http/Controllers/ParcController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parc;
 use Illuminate\Http\Request;
 
 class ParcController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Parc  $parc
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Parc $parc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Parc  $parc
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Parc $parc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Parc  $parc
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Parc $parc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Parc  $parc
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Parc $parc)
+    {
+        //
+    }
 }
